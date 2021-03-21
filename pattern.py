938ea9c93295fff354f8cd522a41774cce3ab684
Moksh Upadhyay <Mokshupadhyay57@github.com>
2021-03-21 13:21:12 +0000

    Initial commit

---
+++ pattern.py
@@ -0,0 +1,18 @@
+# Author: Moksh Upadhyay
+# Date: 21/03/2021
+# Print the following pattern
+# * * *
+# *   *
+# * * *
+m= 3
+n= 3
+for i in range(1, m+1):
+        for j in range(1, n+1):
+            if(i == 1 or i == n or j==1 or j == m):
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+
+
+    
